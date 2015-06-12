dafb61ccc1ad8d5e8bfa8b853930e5ea7dc8ab2d
Tim <thogg4@gmail.com>
2015-06-12 11:36:31 -0600

    add build option and use exits

--- lib/deploy/deployer.rb
+++ lib/deploy/deployer.rb
@@ -9,29 +9,36 @@ module Deploy
     end
 
     method_option :version, aliases: '-v', desc: 'Version'
+    method_option :build, aliases: '-b', desc: 'Build Image'
     desc 'deploy', 'deploy'
     def deploy
       check_setup
 
+      build = options[:build]
+
       version = options[:version]
+      (shout('You must pass a version with -v'); exit(1)) unless version
+
       repo = ENV['DOCKER_REPO']
       
-      build_image(repo, version)
+      if build
+        build_image(repo, version)
 
-      tag_image_as_latest(repo, version)
+        tag_image_as_latest(repo, version)
 
-      push_image(repo, version)
-      push_image(repo, 'latest')
+        push_image(repo, version)
+        push_image(repo, 'latest')
+      end
 
       run_deploy(version)
-
-      save_version(version)
     end
 
     method_option :version, aliases: '-v', desc: 'Version'
     desc 'rollback', 'rollback'
     def rollback
       version = options[:version]
+      (shout('You must pass a version with -v'); exit(1)) unless version
+
       repo = ENV['DOCKER_REPO']
 
       tag_image_as_latest(repo, version)
@@ -39,8 +46,6 @@ module Deploy
       push_image(repo, 'latest')
 
       run_deploy(version)
-
-      save_version(version)
     end
 
     no_commands do
@@ -48,40 +53,40 @@ module Deploy
       def build_image(repo, tag)
         shout "Building Docker Image: #{repo}:#{tag}"
         command = "docker build -t #{repo}:#{tag} ."
-        system(command)
+        exit(1) unless system(command)
       end
 
       def tag_image_as_latest(repo, tag)
         shout "Tagging #{tag} Docker Image as Latest"
         command = "docker tag -f #{repo}:#{tag} #{repo}:latest"
-        system(command)
+        exit(1) unless system(command)
       end
 
       def push_image(repo, tag)
         shout "Pushing Docker Image: #{repo}:#{tag}"
         command = "docker push #{repo}:#{tag}"
-        system(command)
+        exit(1) unless system(command)
       end
 
       def run_deploy(version)
         command = "eb deploy --label #{version}"
-        system(command)
+        exit(1) unless system(command)
       end
 
       def save_version(version)
-        config_file['current_version'] = version
+        #config_file['current_version'] = version
       end
 
       def config_file
-        UserConfig.new('.eb-deploy')['versions']
+        #UserConfig.new('.eb-deploy')['versions']
       end
 
       def check_setup
-        (shout('docker not installed'); return false) unless command?('docker')
-        (shout('eb command not installed'); return false) unless command?('eb')
-        (shout('elasticbeanstalk not configured. run setup command'); return false) unless File.exist?('.elasticbeanstalk')
-        (shout('aws not configured. run setup command'); return false) unless File.exist?(File.expand_path('~/.aws/config'))
-        (shout('ENV DOCKER_REPO not set'); return false) unless ENV['DOCKER_REPO']
+        (shout('docker not installed'); exit(1)) unless command?('docker')
+        (shout('eb command not installed'); exit(1)) unless command?('eb')
+        (shout('elasticbeanstalk not configured. run setup command'); exit(1)) unless File.exist?('.elasticbeanstalk')
+        (shout('aws credentials not configured.'); exit(1)) unless File.exist?(File.expand_path('~/.aws/config'))
+        (shout('ENV DOCKER_REPO not set'); exit(1)) unless ENV['DOCKER_REPO']
       end
 
       def command?(command)
